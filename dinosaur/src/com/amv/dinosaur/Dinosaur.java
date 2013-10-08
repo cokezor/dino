e51983bb2a45f4f35206b6a6577b8b8f589d8893
Leon Lee <93leonlee@gmail.com>
2013-10-08 00:13:59 -0400

    moved dino's texture to its class'

--- dinosaur/src/com/amv/dinosaur/Dinosaur.java
+++ dinosaur/src/com/amv/dinosaur/Dinosaur.java
@@ -1,5 +1,7 @@
 package com.amv.dinosaur;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
@@ -8,13 +10,13 @@ public class Dinosaur {
 	Vector2 position = new Vector2();
 	Vector2 velocity = new Vector2();
 	Rectangle bounds = new Rectangle();
+	Texture image;
 
 	public Dinosaur(Vector2 position){
 		bounds.height = 64;
 		bounds.width = 64;
 		this.position.x = position.x;
 		this.position.y = position.y;
+		image = new Texture(Gdx.files.internal("images/dinosaur.png"));
 	}
-	
-	
 }
--- dinosaur/src/com/amv/dinosaur/GameScreen.java
+++ dinosaur/src/com/amv/dinosaur/GameScreen.java
@@ -8,64 +8,69 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen implements Screen, InputProcessor {
 	Dinosaur dino;
 	final DinosaurGame game;
-	Texture dinoImage;
-	Texture background;
 	DinoController controller;
 	private OrthographicCamera camera;
 	private TiledMap map;
+	private OrthogonalTiledMapRenderer renderer;
 	
 	public GameScreen(final DinosaurGame game){
 		this.game = game;
 		camera = new OrthographicCamera();
 		//set the size of the camera
-		camera.setToOrtho(false, DinosaurGame.WIDTH, DinosaurGame.HEIGHT);
+		//camera.setToOrtho(false, DinosaurGame.WIDTH, DinosaurGame.HEIGHT);
 		//set the dinosaur @ middle of the screen
-		dino = new Dinosaur(new Vector2(DinosaurGame.WIDTH / 2, 20));
+		dino = new Dinosaur(new Vector2(DinosaurGame.WIDTH / 2, 100));
 		controller = new DinoController(dino);
-		dinoImage = new Texture(Gdx.files.internal("images/dinosaur.png"));
-		background = new Texture(Gdx.files.internal("images/background.png"));
 	}
 	@Override
 	public void render(float delta) {
 		//clear screen
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		System.out.println(camera.position.x + " " + camera.position.y);
-		System.out.println(dino.position.x + " " + dino.position.y);
+//		System.out.println(camera.position.x + " " + camera.position.y);
+//		System.out.println(dino.position.x + " " + dino.position.y);
+		//render the map
+		renderer.render();
+		renderer.setView(camera);
+		
 		camera.position.set(dino.position.x + 32, 160, 0);
 		camera.update();
+		
 		controller.update(delta);
+		
 		//use projection matrix specified by camera
-		game.batch.setProjectionMatrix(camera.combined);
-	
+		renderer.getSpriteBatch().setProjectionMatrix(camera.combined);
 		//record all drawing commands from begin
-		game.batch.begin();
-		game.batch.draw(background, 0, 0);
-		game.batch.draw(dinoImage, dino.position.x, dino.position.y);
+		renderer.getSpriteBatch().begin();
+		renderer.getSpriteBatch().draw(dino.image, dino.position.x, dino.position.y);
 		//send all sprites to be rendered
-		game.batch.end();
+		renderer.getSpriteBatch().end();
 		
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
+		camera.viewportHeight = height;
+		camera.viewportWidth = width;
+		camera.update();
 	}
 
 	@Override
 	public void show() {
 		Gdx.input.setInputProcessor(this);
+		map = new TmxMapLoader().load("map/map.tmx");
+		renderer = new OrthogonalTiledMapRenderer(map);
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-		
+		dispose();
 	}
 
 	@Override
@@ -83,7 +88,8 @@ public class GameScreen implements Screen, InputProcessor {
 	@Override
 	public void dispose() {
 		Gdx.input.setInputProcessor(null);
-		
+		map.dispose();
+		renderer.dispose();
 	}
 	@Override
 	public boolean keyDown(int keycode) {
